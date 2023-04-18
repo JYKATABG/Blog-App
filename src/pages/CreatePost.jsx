eb1a963e9140b608bfc167472bff508aef954a94
Jykata <jykatabg@gmail.com>
2023-04-19 00:37:39 +0300

    CreatePost template and css for login and createPost pages

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,7 +1,18 @@
 export const CreatePost = () => {
   return (
-    <div>
-      <h1>Create Posts</h1>
+    <div className="create-post">
+      <div className="cp-container">
+        <h1>Create Posts</h1>
+        <div className="post-title">
+          <label htmlFor="title">Title:</label>
+          <input type="text" placeholder="Post title..." />
+        </div>
+        <div className="post-title">
+          <label htmlFor="information">Post information:</label>
+          <textarea placeholder="Main information..."></textarea>
+        </div>
+        <button>Submit Post</button>
+      </div>
     </div>
   );
 };
