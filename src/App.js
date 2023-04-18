64232b4252264f0a720aff5630a50c942124b969
Jykata <jykatabg@gmail.com>
2023-04-18 20:10:30 +0300

    Added create, login and home pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+//Pages elements
+import { Home } from './pages/Home.jsx'
+import { Login } from './pages/Login.jsx'
+import { CreatePost } from './pages/CreatePost.jsx'
+
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/create-post' element={<CreatePost />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/CreatePost.jsx
@@ -0,0 +1,7 @@
+export const CreatePost = () => {
+  return (
+    <div>
+      <h1>Create Posts</h1>
+    </div>
+  );
+};
